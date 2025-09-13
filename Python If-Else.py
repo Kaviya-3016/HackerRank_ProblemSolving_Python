bac454200e7481ae1d1d05de9ae5a587d02560d2
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-13 09:36:05 +0530

    Update Python If-Else.py

--- Python If-Else.py
+++ Python If-Else.py
@@ -8,17 +8,30 @@ import sys
 
 
 
+# This ensures that the code only runs when the file is executed directly,
+# and not when imported into another file.
 if __name__ == '__main__':
-    n = int(input().strip())
-    if(n%2!=0):
-        print("Weird")
-    elif(n%2==0 and n<=5):
-        print("Not Weird")
-    elif(n%2==0 and n<=20):
-        print("Weird")
-    else:
-        print("Not Weird")
     
+    # Take an integer input from the user
+    n = int(input().strip())  
+    
+    # Condition 1: If n is odd (not divisible by 2)
+    if n % 2 != 0:
+        print("Weird")  
+    
+    # Condition 2: If n is even and in the range 2 to 5
+    elif n % 2 == 0 and n <= 5:
+        print("Not Weird")  
+    
+    # Condition 3: If n is even and in the range 6 to 20
+    elif n % 2 == 0 and n <= 20:
+        print("Weird")  
+    
+    # Condition 4: If n is even and greater than 20
+    else:
+        print("Not Weird")  
+
           
             
             
+
