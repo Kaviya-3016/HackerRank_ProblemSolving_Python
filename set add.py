00026d706853990c8a42df70c701bab65c3f208c
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-16 20:46:38 +0530

    Update set add.py

--- set add.py
+++ set add.py
@@ -1,8 +1,14 @@
-n=int(input())
-stamp=set()
+# Read the number of inputs (total number of country stamps)
+n = int(input())
 
+# Create an empty set to store unique country names
+# A set automatically removes duplicates
+stamp = set()
+
+# Loop through 'n' times to read each country name
 for i in range(n):
-    country=input()
-    stamp.add(country)
+    country = input()     # take a country name as input
+    stamp.add(country)    # add it to the set (duplicates are ignored automatically)
 
+# Finally, print the total number of unique countries
 print(len(stamp))
