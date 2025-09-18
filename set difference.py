4a93f94a98f4c310509b65e8c3bf0e734dc8dd69
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-18 23:06:46 +0530

    Add files via upload

---
+++ set difference.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+M = int(input())              
+myset = set(map(int, input().split()))
+#print(myset)
+N = int(input())              
+myset1 = set(map(int, input().split()))
+#print(myset1)
+students=myset.difference(myset1)
+print(len(students))
