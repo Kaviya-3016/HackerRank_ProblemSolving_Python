5594a0fff5a7ddbd1506f4005af9cb296ba58f97
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-18 23:07:40 +0530

    Update set difference.py

--- set difference.py
+++ set difference.py
@@ -1,10 +1,20 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
+# Read the number of elements in the first set
 M = int(input())              
+
+# Read M space-separated integers, convert them to a set (removes duplicates)
 myset = set(map(int, input().split()))
-#print(myset)
+# Example: if input = "1 2 2 3", myset = {1, 2, 3}
+
+# Read the number of elements in the second set
 N = int(input())              
+
+# Read N space-separated integers, convert them to a set
 myset1 = set(map(int, input().split()))
-#print(myset1)
-students=myset.difference(myset1)
+# Example: if input = "3 4 5", myset1 = {3, 4, 5}
+
+# Difference operation: elements present in myset but NOT in myset1
+students = myset.difference(myset1)
+# Example: {1, 2, 3} - {3, 4, 5} = {1, 2}
+
+# Print the number of such elements
 print(len(students))
