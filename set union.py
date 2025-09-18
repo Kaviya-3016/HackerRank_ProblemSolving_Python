e373bf10c6f4f33ab01bdb2749619a00e906ff9b
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-18 23:03:54 +0530

    Add files via upload

---
+++ set union.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+M = int(input())              
+myset = set(map(int, input().split()))
+#print(myset)
+N = int(input())              
+myset1 = set(map(int, input().split()))
+#print(myset1)
+students=myset.union(myset1)
+print(len(students))
