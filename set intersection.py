bdff13df4fee03ef7ca9ea95932abac445f84992
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-18 23:09:01 +0530

    Update set intersection.py

--- set intersection.py
+++ set intersection.py
@@ -1,9 +1,20 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Read the number of elements in the first set
 M = int(input())              
+
+# Read M space-separated integers, convert them into a set (duplicates removed)
 myset = set(map(int, input().split()))
-#print(myset)
+# Example: if input = "1 2 2 3", then myset = {1, 2, 3}
+
+# Read the number of elements in the second set
 N = int(input())              
+
+# Read N space-separated integers, convert them into a set
 myset1 = set(map(int, input().split()))
-#print(myset1)
-students=myset.intersection(myset1)
+# Example: if input = "3 4 5", then myset1 = {3, 4, 5}
+
+# Intersection: elements that are common to both sets
+students = myset.intersection(myset1)
+# Example: {1, 2, 3} ∩ {3, 4, 5} = {3}
+
+# Print the number of elements in the intersection set
 print(len(students))
