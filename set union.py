ecd86da199969ed9b27e84b3e4d3a7e405f2a322
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-18 23:05:09 +0530

    Update set union.py

--- set union.py
+++ set union.py
@@ -1,9 +1,21 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Read the number of elements in the first set
 M = int(input())              
+
+# Read M space-separated integers, convert them to a set (to remove duplicates automatically)
 myset = set(map(int, input().split()))
-#print(myset)
+# Example: if input = "1 2 2 3", myset = {1, 2, 3}
+
+# Read the number of elements in the second set
 N = int(input())              
+
+# Read N space-separated integers, convert them to a set
 myset1 = set(map(int, input().split()))
-#print(myset1)
-students=myset.union(myset1)
+# Example: if input = "3 4 5", myset1 = {3, 4, 5}
+
+# Union operation: combines both sets, keeping only unique values
+students = myset.union(myset1)
+# Example: {1, 2, 3} U {3, 4, 5} = {1, 2, 3, 4, 5}
+
+# Print the number of unique elements in the union set
 print(len(students))
+# Output will be the count of unique elements across both sets
