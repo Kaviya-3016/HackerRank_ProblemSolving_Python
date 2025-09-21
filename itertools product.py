ea17ad7c7871b6782998599f6900f53f50bec287
Kaviya Murugan <kaviyamurugan3016@gmail.com>
2025-09-21 22:36:25 +0530

    Update itertools product.py

--- itertools product.py
+++ itertools product.py
@@ -1,7 +1,18 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Importing product from itertools
+# 'product' generates the Cartesian product of input iterables
 from itertools import product
 
-A=tuple(map(int, input().split()))
-B=tuple(map(int, input().split()))
-iter=product(A,B)
+# Read first line of input (space-separated integers) and convert to a tuple
+A = tuple(map(int, input().split()))
+
+# Read second line of input (space-separated integers) and convert to a tuple
+B = tuple(map(int, input().split()))
+
+# Generate the Cartesian product of A and B
+# Example: if A = (1, 2) and B = (3, 4), 
+# product(A, B) → (1, 3), (1, 4), (2, 3), (2, 4)
+iter = product(A, B)
+
+# Print all pairs from Cartesian product in a single line
+# '*' unpacks the iterable into separate elements
 print(*iter)
